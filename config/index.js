7b206e9ec12276c498d6266235688c94ea9a0658
crownprince <378097217@qq.com>
2018-05-20 23:38:16 +0800

    修改ip

--- config/index.js
+++ config/index.js
@@ -13,8 +13,10 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '120.78.128.119', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    // disableHostCheck: true,
+    // public:'192.168.28.1:8080',
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
